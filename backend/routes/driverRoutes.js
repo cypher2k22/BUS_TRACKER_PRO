034d15d08ff76597f9a043127d03669462423c40
kushalinisatheeswaran <kushalinisatheeswaran33@gmail.com>
2026-02-21 15:23:49 +0530

    Add driverController  driverRoutes

---
+++ backend/routes/driverRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const driverController = require('../controllers/driverController');
+const authenticateUser = require('../middlewares/authMiddleware');
+const authorizeRole = require('../middlewares/roleMiddleware');
+// All routes require driver authentication
+router.use(authenticateUser);
+router.use(authorizeRole("driver"));
+// Get today’s trips
+router.get('/trips/today', driverController.getTodayTrips);
+
+// Start a trip
+router.post('/trips/:tripId/start', driverController.startTrip);
+
+// Send live location
+router.post('/trips/:tripId/ping-location', driverController.pingLocation);
+
+// Stop trip
+router.post('/trips/:tripId/stop', driverController.stopTrip);
+
+// Get tracking status
+router.get('/trips/status', driverController.getStatus);
+
+module.exports = router;

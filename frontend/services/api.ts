c75e0e5dc3d5853f2d5d2443ab1beda6614e936c
kushalinisatheeswaran <kushalinisatheeswaran33@gmail.com>
2026-02-23 21:35:24 +0530

    Save local changes before rebase

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // change to your ip address and port number where the backend server is running
-const API_URL = 'http://172.20.10.5:3000'; 
+const API_URL = 'http:// 192.168.56.1:3000'; 
 
 const api = axios.create({
   baseURL: API_URL,
